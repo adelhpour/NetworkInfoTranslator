6872dab0d1f97aa3a5c6260976fedbb80a6a979d
adelhpour <ad.heydarabadi@gmail.com>
2023-11-27 12:37:31 -0800

    the example is commneted

--- src/networkinfotranslator/network_info_translator.py
+++ src/networkinfotranslator/network_info_translator.py
@@ -6,12 +6,13 @@ from exports.export_cytoscapejs import NetworkInfoExportToCytoscapeJs
 from exports.export_figure_matplotlib import NetworkInfoExportToMatPlotLib
 from exports.export_escher import NetworkInfoExportToEscher
 
+"""
 sbml_graph_info = NetworkInfoImportFromNetworkEditor()
 sbml_graph_info.extract_info("/Users/home/Downloads/Model.json")
 sbml_export = NetworkInfoExportToMatPlotLib()
 sbml_export.extract_graph_info(sbml_graph_info)
 sbml_export.export("/Users/home/Downloads/Model.json")
-"""
+
 sbml_graph_info = NetworkInfoImportFromNetworkEditor()
 f = open("/Users/home/Downloads/network7.json")
 sbml_graph_info.extract_info(json.load(f))
